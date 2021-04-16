9d5d3cc713d86e46b79a53364fbed8b71b78b349
Viboll <thachviboll@gmail.com>
2021-04-15 17:07:04 -0700

    Made significant progress towards completing the event creation viewcontroller and the tableview for event page

--- Assemble/CreateEventVC.swift
+++ Assemble/CreateEventVC.swift
@@ -11,11 +11,27 @@ class CreateEventVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var eventNameInput: UITextField!
+    
+    @IBOutlet weak var descriptionInput: UITextField!
+    
+    @IBOutlet weak var locationInput: UITextField!
+    
+    @IBOutlet weak var dateInput: UITextField!
+    
+    @IBAction func createEventButton(_ sender: Any) {
+        performSegue(withIdentifier: "createEventSegue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "createEventSegue") {
+            let vc = segue.destination as! OpenGroupVC
+            // need to pass in the user input to event page
+        }
+    }
     /*
     // MARK: - Navigation
 
--- Assemble/OpenGroupVC.swift
+++ Assemble/OpenGroupVC.swift
@@ -8,14 +8,49 @@
 import UIKit
 
 class eventCell: UITableViewCell {
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
 }
-class OpenGroupVC: UIViewController {
 
+
+class OpenGroupVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func createEventButton(_ sender: Any) {
+        performSegue(withIdentifier: "createEventPageSegue", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.register(UINib(nibName: "eventCell", bundle: nil), forCellReuseIdentifier: "eventID")
         // Do any additional setup after loading the view.
+    }
+    
+    var eventNameList = [String]()
+    var descriptionList = [String]()
+    var locationList = [String]()
+    var dateList = [String]()
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventNameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: eventCell = tableView.dequeueReusableCell(withIdentifier: "eventID", for: indexPath) as! eventCell
+        cell.eventNameLabel.text = eventNameList[indexPath.row]
+        cell.descriptionLabel.text = descriptionList[indexPath.row]
+        cell.locationLabel.text = locationList[indexPath.row]
+        cell.dateLabel.text = dateList[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 240.0
     }
     
 
